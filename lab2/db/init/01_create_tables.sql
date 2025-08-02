7eb2e1cbb6e5358f4b9a3c83bf8f4d348d2bb501
Quentin Cartier <daoliangshumaishuji@gmail.com>
2025-08-02 06:40:37 +0200

    Update table init

--- lab2/db/init/01_create_tables.sql
+++ lab2/db/init/01_create_tables.sql
@@ -10,3 +10,25 @@ CREATE TABLE DimDate (
     Weekday INT,
     WeekdayName VARCHAR(10)
 );
+
+CREATE TABLE DimStation (
+    Stationid INT PRIMARY KEY,
+    City VARCHAR(50)
+);
+
+CREATE TABLE DimTruck (
+    Truckid INT PRIMARY KEY,
+    TruckType VARCHAR(20)
+);
+
+CREATE TABLE FactTrips (
+    Tripid INT PRIMARY KEY,
+    Dateid INT,
+    Stationid INT,
+    Truckid INT,
+    Wastecollected DECIMAL(6,2),
+
+    FOREIGN KEY (Dateid) REFERENCES DimDate(dateid),
+    FOREIGN KEY (Stationid) REFERENCES DimStation(Stationid),
+    FOREIGN KEY (Truckid) REFERENCES DimTruck(Truckid)
+);
